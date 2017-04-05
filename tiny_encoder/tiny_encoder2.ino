f52029032a897c26e1ee888aabd58d2d9ec3a7c3
vidmo91 <mateuszwidomski@gmail.com>
2017-04-05 20:13:32 +0200

    tiny encoder triggered by serial value

---
+++ tiny_encoder/tiny_encoder2.ino
@@ -0,0 +1,33 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(3, 4);
+volatile uint8_t lastEncoded = 0;
+uint8_t encoded;
+volatile uint8_t encoderValue = 0;
+uint8_t lastencoderValue = 0;
+uint8_t MSB;
+uint8_t LSB;
+uint8_t sum;
+
+void setup()
+{
+  mySerial.begin(9600);
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+}
+void loop()
+{
+
+  while (mySerial.available()) {
+    uint8_t inc=mySerial.read();
+    if ( inc == 'b') 
+      mySerial.println(encoderValue, DEC);
+  }
+  encoded = (digitalRead(0) << 1) | digitalRead(1); 
+  sum  = (lastEncoded << 2) | encoded;
+  if (sum == 0b0100 || sum == 0b0010)
+    encoderValue ++;
+  if (sum == 0b1110 || sum == 0b0111)
+    encoderValue --;
+  lastEncoded = encoded;
+}

17e95fea2fd7c26343ac33d1711a02919ce21498
vidmo91 <mateuszwidomski@gmail.com>
2017-04-06 20:02:47 +0200

    minor changes

--- tiny_encoder2/tiny_encoder2.ino
+++ tiny_encoder2/tiny_encoder2.ino
@@ -8,20 +8,25 @@ uint8_t lastencoderValue = 0;
 uint8_t MSB;
 uint8_t LSB;
 uint8_t sum;
+uint8_t button=97;
 
 void setup()
 {
   mySerial.begin(9600);
   pinMode(0, INPUT_PULLUP);
   pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
 }
 void loop()
 {
 
   while (mySerial.available()) {
     uint8_t inc=mySerial.read();
-    if ( inc == 'b') 
-      mySerial.write(encoderValue, DEC);
+    if ( inc == 'a') {
+      mySerial.write(encoderValue);
+      mySerial.write(button);
+      button=97;
+    }
   }
   encoded = (digitalRead(0) << 1) | digitalRead(1); 
   sum  = (lastEncoded << 2) | encoded;
@@ -30,4 +35,7 @@ void loop()
   if (sum == 0b1110 || sum == 0b0111)
     encoderValue --;
   lastEncoded = encoded;
+  if(digitalRead(2)==0)
+    button=99;
+  
 }
